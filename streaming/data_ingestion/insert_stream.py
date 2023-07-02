d043ee70ee7b559212eaef031ecfe5534cfdcc68
Pranav Bawa <137123100+pbawa8@users.noreply.github.com>
2023-07-02 13:26:31 -0700

    Update insert_stream.py

--- streaming/data_ingestion/insert_stream.py
+++ streaming/data_ingestion/insert_stream.py
@@ -6,7 +6,7 @@ df = pd.read_csv('test_data_sample.csv', sep=',')
 df.head(10)
 
 # AWS API endpoint
-URL = 'https://yd68muw1l7.execute-api.us-east-2.amazonaws.com/ecom_prod/test'
+URL = 'https://*****.execute-api.us-east-2.amazonaws.com/ecom_prod/test'
 
 for i in df.index:
 
